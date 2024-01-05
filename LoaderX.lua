7bac696bb64fc1dabce98e1c665b676343675284
YxyO1 <152443325+YxyO1@users.noreply.github.com>
2024-01-05 22:44:32 +0100

    Update LoaderX.lua

--- LoaderX.lua
+++ LoaderX.lua
@@ -1,14 +1,1 @@
-local placeId = game.PlaceId
 
-local scripts = {
-    ["7346416636"] = "https://raw.githubusercontent.com/YxyO1/Hubs/main/PopItHub.lua",
-    ["7903991471"] = "https://raw.githubusercontent.com/YxyO1/Hubs/main/PetTrading.lua"
-}
-
-local scriptToLoad = scripts[tostring(placeId)]
-
-if scriptToLoad then
-    loadstring(game:HttpGet(scriptToLoad))()
-else
-    warn("Script not found for PlaceId:", placeId)
-end
